c15bcafa1c6b3e2281287e154b8e2be44a0eca8f
Kumar Sahil <krsahil8825@gmail.com>
2025-04-18 22:44:28 +0530

    created login and signup page and updated class names

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-function showPassword() {
+function showPasswordsignin() {
 	let password = document.getElementById("password");
 	let showpassword = document.getElementById("showpassword");
 
@@ -7,6 +7,22 @@ function showPassword() {
 		showpassword.innerHTML = "Hide Password";
 	} else {
 		password.type = "password";
+		showpassword.innerHTML = "Show Password";
+	}
+}
+
+function showPasswordsignup() {
+	let password = document.getElementById("password");
+	let confirmPassword = document.getElementById("confirmPassword");
+	let showpassword = document.getElementById("showpassword");
+
+	if (password.type === "password") {
+		password.type = "text";
+		confirmPassword.type = "text";
+		showpassword.innerHTML = "Hide Password";
+	} else {
+		password.type = "password";
+		confirmPassword.type = "password";
 		showpassword.innerHTML = "Show Password";
 	}
 }
